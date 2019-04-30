11ffbb13cf3a1e66f3015f0e9ea315ffc2a81c50
Alan dos Santos Nunes Bernd <adsnb.alan@gmail.com>
2019-04-30 20:29:50 -0300

    Single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: []
-    // tags: ['tag 1', 'tag 2', 'tag 3']
-  };
-
-  handleIncrement = counter => {
-    this.setState({ value: this.state.value + counter });
-  };
-
   render() {
     return (
       <React.Fragment>
@@ -19,10 +9,16 @@ class Counter extends Component {
         </span>
 
         <button
-          onClick={() => this.handleIncrement(1)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         <br />
       </React.Fragment>
@@ -31,12 +27,12 @@ class Counter extends Component {
 
   getBadgesClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
